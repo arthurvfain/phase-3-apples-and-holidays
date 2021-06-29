12c0886d5a6eac4768c467446405af59148e2353
Arthur V Fain <arthurvfain@gmail.com>
2021-06-29 16:53:52 -0400

    commit tree 815705cd3fdc10ecd1fc1b10a73c99c80ad14aa0

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,32 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].values.collect {|holiday| holiday << supply}
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season.to_sym][holiday_name.to_sym] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  winter_supplies = holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,7 +54,18 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.keys.each do |season|
+    puts season.to_s.capitalize + ":"
+    holiday_hash[season].keys.each do |holiday|
+      holiday_line = ""
+      holiday_start = "  " + holiday.to_s.split("_").collect{|word|word.capitalize}.join(" ") + ": "
+      holiday_line << holiday_start
+      holiday_hash[season][holiday].each do |supply|
+        holiday_line << supply + ", "
+      end
+      puts holiday_line[0..-3]
+    end 
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
@@ -62,9 +74,18 @@ def all_holidays_with_bbq(holiday_hash)
 
 end
 
-
-
-
-
-
-
+all_supplies_in_holidays({
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+})
